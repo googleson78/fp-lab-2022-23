fca02d81c926955bfdbf170bc2b4a8ca8c050b7e
Georgi Lyubenov <godzbanebane@gmail.com>
2022-11-09 14:19:39 +0200

    Add warnings to (4)ListMaybe and (3)HOF

--- exercises/04/ListMaybe.hs
+++ exercises/04/ListMaybe.hs
@@ -1,3 +1,14 @@
+-- cover all cases!
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
+-- warn about incomplete patterns v2
+{-# OPTIONS_GHC -fwarn-incomplete-uni-patterns #-}
+-- write all your toplevel signatures!
+{-# OPTIONS_GHC -fwarn-missing-signatures #-}
+-- use different names!
+{-# OPTIONS_GHC -fwarn-name-shadowing #-}
+-- use all your pattern matches!
+{-# OPTIONS_GHC -fwarn-unused-matches #-}
+
 module ListMaybe where
 
 import Prelude hiding (all, and, concat, drop, filter, length, map, null, product, reverse, subtract, sum, take, zip, zipWith, (++))
