975866c73261d828b3145a27cd0f51bb3b106060
Georgi Lyubenov <godzbanebane@gmail.com>
2023-01-17 14:20:21 +0200

    Add 12

---
+++ exercises/12/ParserTodo.hs
@@ -0,0 +1,12 @@
+-- this file mostly exists so we can record that we spent two exercises on using parsers
+
+
+
+
+
+-- remind homework
+
+-- reminder review
+
+-- mention many some
+-- show mapping, liftA2, analogues to maybe/[]
